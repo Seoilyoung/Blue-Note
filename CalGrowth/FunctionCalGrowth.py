aa965c47b5b17209e3fac2dc1589f5aa5c091fd2
서일영 <seo92iyiu@gmail.com>
2023-02-22 23:21:59 +0900

    CalGrowth JSON 생성

--- CalGrowth/FunctionCalGrowth.py
+++ CalGrowth/FunctionCalGrowth.py
@@ -1,2 +1,11 @@
-# db연결
-# mysql로 하는게 편한가?
+import json
+
+with open('CalGrowth/Database.json','r',encoding='UTF-8') as f:
+    json_datas = json.load(f)
+print(json.dumps(json_datas, ensure_ascii=False))
+
+sum_note = 0
+for json_data in json_datas :
+    # if json_data['Academy']=='밀레니엄':
+        sum_note +=json_data['Skill_Note']['Main'][2]
+print(sum_note)
